3406fd18ee60aafc7b04b4146b8a536a7fb4c60c
Rangachari Anand <rxanand@gmail.com>
2022-12-12 16:09:05 -0500

    Reached Logix conditional

--- extract_logix.py
+++ extract_logix.py
@@ -6,11 +6,40 @@
 import xml.etree.ElementTree as ET
 import argparse
 
+def getAllLogix(root):
+    queryString = '.logixs/logix'
+    return root.findall(queryString)
+
+def getConditionalBySystemName(root, systemName):
+    queryString = ".conditionals/conditional/systemName[.='%s']/.." % systemName
+    conditionals = root.findall(queryString)
+    if len(conditionals) != 1:
+        return None
+    else:
+        return conditionals[0]
+
+def processLogixConditional(root, logixSystemName, logixUserName, conditionalSystemName):
+    conditional = getConditionalBySystemName(root, conditionalSystemName)
+    print( conditional.attrib['userName'], conditional.attrib['systemName'])
+
 def main(args):
     ifn = args.inputFile
     tree = ET.parse(ifn)
     root = tree.getroot()
-    
+    logixs = getAllLogix(root)
+    for logix in logixs:
+        print('======')
+        logixUserName = ''
+        logixSystemName = ''
+        for child in logix:
+            if child.tag == 'userName':
+                logixUserName = child.text
+            elif child.tag == 'systemName':
+                logixSystemName = child.text
+            elif child.tag == 'logixConditional':
+                conditionalSystemName = child.attrib['systemName']
+                processLogixConditional(root, logixSystemName, logixUserName, conditionalSystemName)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Deconstruct a JMRI XML formatted layout description file')
