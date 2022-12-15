934dc2844c4e8b3331e1a3c8aab5fae116632a7f
Rangachari Anand <rxanand@gmail.com>
2022-12-15 18:28:33 -0500

    Fixed numerous small bugs, especially related to computing the guard set

--- jylogix.py
+++ jylogix.py
@@ -7,6 +7,8 @@ def getEntryExit(name):
     for d in destinations:
         if d.getUserName() == name:
             return d
+        elif d.getSystemName() == name:
+            return d
     return None
 
 # Retrieve event generating objects referenced in this Logix and
@@ -17,48 +19,54 @@ def attachListener(logix, listener):
     # listeners attached. Don't want to attach duplicate
     # listeners!
     previouslyHandled = set()
-    for l in logix:
+    for conditional in logix:
         guardSet = set()
-        l['guardSet'] = guardSet
-        for g in l['guard']:
+        conditional['guardSet'] = guardSet
+        for g in conditional['guard']:
             otype = g[0]
             oid = g[1]
-            #print('Object type: ' + otype + ' Object id: ' + oid)
-            if (otype, oid) in previouslyHandled:
-                #print('Skipping ' + otype + ' ' + oid)
-                pass
-            else:
-                previouslyHandled.add((otype, oid))
-                if otype == 'Sensor':
-                    sensor = sensors.getSensor(oid)
-                    if sensor is None:
-                        print('ERROR Could not locate sensor ' + oid)
+            if otype == 'Sensor':
+                sensor = sensors.getSensor(oid)
+                if sensor is None:
+                    print('ERROR Could not locate sensor ' + oid)
+                else:
+                    guardSet.add(sensor.getSystemName())
+                    if (otype, oid) in previouslyHandled:
+                        pass
                     else:
                         sensor.addPropertyChangeListener(listener)
-                        guardSet.add(sensor.getSystemName())
-                        #print('Attached listener to sensor ' + oid)
-                elif otype == 'Turnout':
-                    turnout = turnouts.getTurnout(oid)
-                    if turnout is None:
-                        print('ERROR Could not locate turnout ' + oid)
+                        previouslyHandled.add((otype, oid))
+                        print('Attached listener to sensor ' + oid)
+            elif otype == 'Turnout':
+                turnout = turnouts.getTurnout(oid)
+                if turnout is None:
+                    print('ERROR Could not locate turnout ' + oid)
+                else:
+                    guardSet.add(turnout.getSystemName())
+                    if (otype, oid) in previouslyHandled:
+                        pass
                     else:
                         turnout.addPropertyChangeListener(listener)
-                        guardSet.add(turnout.getSystemName())
-                        #print('Attached listener to turnout' + oid)
-                elif otype == 'EntryExit':
-                    entryExit = getEntryExit(oid)
-                    if entryExit is None:
-                        print('ERROR Could not locate entry exit ' + oid)
+                        previouslyHandled.add((otype, oid))
+                        print('Attached listener to turnout' + oid)
+            elif otype == 'EntryExit':
+                entryExit = getEntryExit(oid)
+                if entryExit is None:
+                    print('ERROR Could not locate entry exit ' + oid)
+                else:
+                    guardSet.add(entryExit.getSystemName())
+                    if (otype, oid) in previouslyHandled:
+                        pass
                     else:
                         entryExit.addPropertyChangeListener(listener)
-                        guardSet.add(entryExit.getSystemName())
-                        #print('Attached listener to entry exit' + oid)
-                else:
-                    print ('ERROR Unknown object type ' + otype)
+                        previouslyHandled.add((otype, oid))
+                        print('Attached listener to entry exit' + oid)
+            else:
+                print ('ERROR Unknown object type ' + otype)
     print('Guard sets')
     for (i, l) in enumerate(logix):
         guards = ' '.join(l['guardSet'])
-        print('Logix ' + str(i) + ' ' + guards)
+        print('Conditional ' + l['id'] + ' Guards:' + guards)
 
 def convertStateToString(state, objectType):
     if objectType == 'Turnout':
@@ -120,7 +128,7 @@ def takeActions(actions):
                     t.state = THROWN
                 else:
                     print('ERROR unknown turnout state ' + a_state)
-                print( 'Setting turnout ' + a_oid + ' to ' + a_state)
+                print( 'Action setting turnout ' + a_oid + ' to ' + a_state)
         elif a_type == 'Sensor':
             s = sensors.getSensor(a_oid)
             if s is None:
@@ -132,23 +140,23 @@ def takeActions(actions):
                     s.state = INACTIVE
                 else:
                     print('ERROR unknown sensor state ' + a_state)
-                print( 'Setting sensor ' + a_oid + ' to ' + a_state )
+                print( 'Action setting sensor ' + a_oid + ' to ' + a_state )
 
 # Sample event
 # java.beans.PropertyChangeEvent[propertyName=KnownState; oldValue=4; newValue=2; propagationId=null; source=IS5001]
 def handleEvent(event, logix):
     sname = event.getSource().getSystemName()
-    print('Source: ' + sname)
-    print('New value: ', + event.getNewValue())
-    for i, l in enumerate(logix):
+    print('Event source: ' + sname + ' New value ' + str(event.getNewValue()))
+    for l in logix:
         if sname in l['guardSet']:
-            print('Event captured by logix ' + str(i))
+            print('Event captured by logix ' + l['id'])
             # Now evaluate the actual conditions
             evaluation = evaluateGuards(l['guard'], l['formula'])
             print('Evaluated to: ' + str(evaluation))
             if evaluation:
                 takeActions(l['action'])
         else:
-            print('Event not captured by logix ' + str(i))
+            #print('Event not captured by logix ' + str(i))
+            pass
 
 
